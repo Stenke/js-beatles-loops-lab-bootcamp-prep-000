30842d41aceb06a02fffec2850a51dcdb17eab1c
Stephen Enke <stephenenke@gmail.com>
2020-08-25 19:11:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(arrayMusician, arrayInstrument) {
   }
   return array;
 }
-function johnLennonFacts(facts) {
+function johnLennonFacts() {
   var facts = ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
