a67dedb5d02c344402e55194ad3ab010c38bfe17
Stephen Enke <stephenenke@gmail.com>
2020-08-25 18:50:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // add solution here
 function theBeatlesPlay(arrayMusician, arrayInstrument) {
+  var emptyArray = [];
   for ( i = 0; i < arrayMusician.length; i++) {
     emptyArray = array.push(arrayMusician[i] + "plays" arrayInstrument[i]);
   }
