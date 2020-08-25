871d44cdc2bd655da2b9c1147fccd21ed1fea8c4
Stephen Enke <stephenenke@gmail.com>
2020-08-25 18:49:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(arrayMusician, arrayInstrument) {
+  for ( i = 0; i < arrayMusician.length; i++) {
+    emptyArray = array.push(arrayMusician[i] + "plays" arrayInstrument[i]);
+  }
+  return emptyArray;
+}
