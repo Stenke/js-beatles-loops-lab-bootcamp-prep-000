90f58619fb81bf7730ff0869b95a4eb9a473f3de
Stephen Enke <stephenenke@gmail.com>
2020-08-25 19:16:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function theBeatlesPlay(arrayMusician, arrayInstrument) {
   return array;
 }
 function johnLennonFacts(facts) {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var array = [];
+  const array = [];
   var i = 0;
   while (array.length < facts.length) {
     array.push(facts[i] + "!!!");
