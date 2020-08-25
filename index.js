1c596b5920a0e8a247e6f6e9c90507dc54345a5c
Stephen Enke <stephenenke@gmail.com>
2020-08-25 18:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(arrayMusician, arrayInstrument) {
   var emptyArray = [];
   for ( i = 0; i < arrayMusician.length; i++) {
-    emptyArray = array.push(arrayMusician[i] + "plays" + arrayInstrument[i]);
+    emptyArray = array.push(arrayMusician[i] + " plays " + arrayInstrument[i]);
   }
   return emptyArray;
 }
